51a37087b1d4c90b0b7a0cb3c406f78238440c23
yamil-arguedas <yamilarguedas19@gmail.com>
2024-05-26 13:29:31 -0600

    V54

--- PORTAFOLIO/PORTAFOLIO/Controllers/HomeController.cs
+++ PORTAFOLIO/PORTAFOLIO/Controllers/HomeController.cs
@@ -16,8 +16,42 @@ namespace PORTAFOLIO.Controllers
         public IActionResult Index()
         {
 
-            
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto> { 
+                new Proyecto
+            {
+                Titulo = "Amazon",
+                Descripcion = "E-commerce realizado en ASP.NET Core",
+                Link = "https://amazon.com/",
+                ImagenURL = "/imagenes/Amazon.png"
+            },
+                 new Proyecto
+            {
+                Titulo = "NewYork Times",
+                Descripcion = "Pagina de noticias en React",
+                Link = "https://nytimes.com/",
+                ImagenURL = "/imagenes/nytimes.png"
+            },
+                  new Proyecto
+            {
+                Titulo = "Reddit",
+                Descripcion = "Red social para compartir en comunidades ",
+                Link = "https://reddit.com/",
+                ImagenURL = "/imagenes/Reddit.png"
+            },
+                   new Proyecto
+            {
+                Titulo = "Steam",
+                Descripcion = "Tienda en linea para comprar videojuegos",
+                Link = "https://store.steampowered.com/",
+                ImagenURL = "/imagenes/steam.png"
+            },
+            };
         }
 
         public IActionResult Privacy()
---
+++ PORTAFOLIO/PORTAFOLIO/Models/HomeIndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PORTAFOLIO.Models
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Proyecto> Proyectos { get; set; }
+    }
+}
