810caec950dc3a12bc1f50a0d0f945b088de639c
yamil-arguedas <yamilarguedas19@gmail.com>
2024-05-26 20:16:11 -0600

    V61

--- PORTAFOLIO/PORTAFOLIO/Controllers/HomeController.cs
+++ PORTAFOLIO/PORTAFOLIO/Controllers/HomeController.cs
@@ -9,21 +9,26 @@ namespace PORTAFOLIO.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
-        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        private readonly IConfiguration configuration;
+
+        public HomeController(ILogger<HomeController> logger,
+            IRepositorioProyectos repositorioProyectos,
+            IConfiguration configuration)
         {
             _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            var apellido = configuration.GetValue<string>("Apellido");
             _logger.LogTrace("Este es un mensaje de trace");
             _logger.LogDebug("Este es un mensaje de debug");
             _logger.LogInformation("Este es un mensaje de information");
             _logger.LogWarning("Este es un mensaje de warning");
             _logger.LogError("Este es un mensaje de error");
-            _logger.LogCritical("Este es un mensaje de critical");
-
+            _logger.LogCritical("Este es un mensaje de crtitical " + apellido);
             var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
--- PORTAFOLIO/PORTAFOLIO/obj/Debug/net6.0/PORTAFOLIO.AssemblyInfo.cs
+++ PORTAFOLIO/PORTAFOLIO/obj/Debug/net6.0/PORTAFOLIO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PORTAFOLIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c1ef4b9b5257eca7ccef238182033053b39a302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff9ee29ffeb4b9dc5e2be9e8105fd15558a2c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("PORTAFOLIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PORTAFOLIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
